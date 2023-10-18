1f8169ae05fad18cab2bf8adfdc629bd839f0b5e
Bateyjosue <josuebatey19@gmail.com>
2023-10-18 10:31:52 +0200

    refactor: password penalty to use reduce function

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b5fad9d (fix Merge)
+
 /**
  * Calculates the sum of penalty points for a given password.
  * Double characters like `aa` count as 1 penalty point, triples and more are 2 points.
@@ -11,49 +8,8 @@
  */
 export default function penaltyPoints(password = "") {
   if (typeof password !== "string") password = String(password);
-  if(password == 'null') return 0
-  
-<<<<<<< HEAD
-  let re = /(.)\1+/g
-  let penalty = 0;
-  let matches = password.match(re);
+  if (password == 'null') return 0
 
-  if(matches){
-    matches.forEach(match => {
-      penalty += match.length >= 3? 2 : 1;
-    })
-  } else penalty = 0
+  return matches.reduce((acc, curr) => curr.length >= 3 ? acc + 2 : acc + 1)
+}
 
-  return penalty;
-}
-=======
-/**
- * Calculates the sum of penalty points for a given password.
- * Double characters like `aa` count as 1 penalty point, triples and more are 2 points.
- * It returns the sum of penalty points for a password or 0.
- * @param {string} password
- * @returns {number}
- */
-export default function penaltyPoints(password = "") {
-  if (typeof password !== "string") password = String(password);
-  if(password == 'null') return 0
-  
-  let regexPattern = /(.)\1+/g
-  let penalty = 0;
-  let matches = password.match(regexPattern);
-    matches?.forEach(match => {
-      penalty += match.length >= 3? 2: 1;
-    })
-  return penalty;
-}
->>>>>>> 304230e (refactor: password penalty)
-=======
-  let regexPattern = /(.)\1+/g
-  let penalty = 0;
-  let matches = password.match(regexPattern);
-    matches?.forEach(match => {
-      penalty += match.length >= 3? 2: 1;
-    })
-  return penalty;
-}
->>>>>>> b5fad9d (fix Merge)
